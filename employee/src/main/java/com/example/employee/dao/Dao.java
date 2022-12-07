5aa4ae8b9713ce9bb8f7d81909bcc967cfc52e47
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-12-07 12:24:55 +0530

    search emp

--- employee/src/main/java/com/example/employee/dao/Dao.java
+++ employee/src/main/java/com/example/employee/dao/Dao.java
@@ -1,7 +1,13 @@
 package com.example.employee.dao;
 
 import com.example.employee.model.EmployeeModel;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface Dao extends CrudRepository<EmployeeModel,Integer> {
+
+    @Query(value = "SELECT `id`, `company`, `designamtion`, `empcode`, `name`, `password`, `phone`, `salary`, `username` FROM `employee_model` WHERE `empcode`= :empcode",nativeQuery = true)
+    List<EmployeeModel> searchEmployee(Integer empcode);
 }
